4a084a8336613d71756f3b6b4329d754e1cf0912
XienX <1324548879@qq.com>
2020-07-14 09:29:23 +0800

    demo1 创建第三方Bean

--- src/main/java/demo1/AppConfig.java
+++ src/main/java/demo1/AppConfig.java
@@ -2,12 +2,22 @@ package demo1;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.ZoneId;
 
 @Configuration
 @ComponentScan
 public class AppConfig { //使用Annotation配置
+
+    // 创建一个Bean:
+    @Bean
+    ZoneId createZoneId() {
+        return ZoneId.of("Z");
+    }
+
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         UserService userService = context.getBean(UserService.class);
