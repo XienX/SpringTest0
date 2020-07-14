cefc5784f99ad51ce9cf2b4c4c3f5c27cf628f0d
XienX <1324548879@qq.com>
2020-07-14 09:25:37 +0800

    demo1 可选注入

--- src/main/java/demo1/MailService.java
+++ src/main/java/demo1/MailService.java
@@ -1,5 +1,6 @@
 package demo1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.ZoneId;
@@ -8,14 +9,16 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class MailService {
+
+    @Autowired(required = false) //可选注入，虽然IDE不推荐写在字段上，但只有这样才起作用
     private ZoneId zoneId = ZoneId.systemDefault();
 
-    public void setZoneId(ZoneId zoneId) {
+    /*public void setZoneId(ZoneId zoneId) {
         this.zoneId = zoneId;
-    }
+    }*/
 
     public String getTime() {
-        return ZonedDateTime.now(this.zoneId).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        return ZonedDateTime.now(zoneId).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
     }
 
     public void sendLoginMail(User user) {
